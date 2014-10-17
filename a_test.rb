915b208f81a15fa6cac0d0fb65cd357f69a4f3c7
ka <ka.kaosf@gmail.com>
2014-10-17 18:14:19 +0900

    Modify from `def m_test ...` to `test "m" do ...`

--- a_test.rb
+++ a_test.rb
@@ -2,12 +2,12 @@ require 'test-unit'
 require './a'
 
 class TestA < Test::Unit::TestCase
-  def test_m
+  test "m" do
     assert_equal 1, A.new.m
   end
 
   sub_test_case "sub test" do
-    def test_mm
+    test "sub m" do
       assert_equal 1, A.new.m
     end
   end
